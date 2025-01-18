bd9d41f4029a341f108e29cf46f39b222a87335b
Minsoo Cho <textign@gmail.com>
2025-01-18 16:24:48 +0900

    마이페이지 링크 추가

--- src/components/auth/Join.tsx
+++ src/components/auth/Join.tsx
@@ -3,7 +3,7 @@ import JoinForm from "./JoinForm";
 function Join() {
   return (
     <div className="w-[600px] mx-auto bg-white rounded-xl py-8 px-10 flex flex-col justify-center items-center gap-8">
-      <h1 className="font-bold text-xl">JOIN</h1>
+      <h1 className="font-bold text-xl">JOINs</h1>
       <JoinForm />
     </div>
   );
--- src/components/layout/GlobalNav.tsx
+++ src/components/layout/GlobalNav.tsx
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../../zustand/useAuthStore";
 
 function GlobalNav() {
+  const user = useAuthStore((state) => state.user);
   return (
     <ul className="flex font-[18px] gap-8">
-      <li>
-        <Link to="/login" className="hover:text-sky-700">
-          로그인
-        </Link>
-      </li>
-      <li className="hover:text-skyblue-700">
-        <Link to="/join" className="hover:text-sky-700">
-          회원가입
-        </Link>
-      </li>
+      {user ? (
+        <li>
+          <Link to="mypage">마이페이지 </Link>
+        </li>
+      ) : (
+        <>
+          <li>
+            <Link to="/login" className="hover:text-sky-700">
+              로그인
+            </Link>
+          </li>
+          <li className="hover:text-skyblue-700">
+            <Link to="/join" className="hover:text-sky-700">
+              회원가입
+            </Link>
+          </li>
+        </>
+      )}
     </ul>
   );
 }
